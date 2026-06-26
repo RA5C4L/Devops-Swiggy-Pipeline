fedd821f51574ede8b00e9789656fb9d38b01a45
Navneet Vivek <Navneet Vivek>
2026-06-26 12:21:47 +0530

    Set SSH command to use local pem file path

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -27,6 +27,6 @@ output "app_url" {
 
 # ========== SSH Command ==========
 output "ssh_command" {
-  description = "SSH command — replace /path/to/ with the full path to your .pem file"
-  value       = "ssh -i /path/to/${var.key_name}.pem ubuntu@${aws_instance.web.public_ip}"
+  description = "SSH command to connect to your instance"
+  value       = "ssh -i \"C:/Users/vivek/OneDrive/Desktop/${var.key_name}.pem\" ubuntu@${aws_instance.web.public_ip}"
 }
